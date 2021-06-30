0babb0a2f0c61b520f8fdec5323a0054e35a0823
Dwaylan <Dwaylan.Applewhite@gmail.com>
2021-06-29 20:20:49 -0400

    finished and hosted the API to vercel

--- index.js
+++ index.js
@@ -10,4 +10,8 @@ const PORT = process.env.PORT || 5000;
 app.use(bodyparser.json());
 app.use(business);
 
+app.get("/", (req, res) => {
+  res.send("vyasa business API");
+});
+
 app.listen(PORT, console.log(`I am listening on port ${5000}`));
